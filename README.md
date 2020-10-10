162e83ce59e1fa3bea0ca4c3452a4cbd911e0278
Lohelani <lohelani.hicks@gmail.com>
2020-10-10 10:12:30 -0400

    highscore edit

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # timed-quiz
+
+#### _Web APIs: Timed Quiz, 10.10.20_
+
+### _By Lohelani Hicks_
+
+## Description and Method
+
+For this project I used javascript to 
+Once the user starts the quiz, present the first question and start the coutdown timer.
+When the user selects an answer, pause the quiz timer and let the user know if the answer right/wrong.
+If the user is wrong, subtract penalty time from the clock.
+After 2 seconds, proceed to the next question and resume the countdown timer.
+If the countdown timer reaches 0 the quiz is over.
+If the user answers all the questions the quiz is over.
+The user's score and initials are then saved after which the high scores will be displayed.
+Also provide a link straight to viewing the high scores page.
+Included links to bootstraps css, and bootstrap javasript in the code.
+
+
+
+## Technologies used
+* Bootstrap
+* CSS 
+* HTML
+* javascript
+
+## Deployed Website
+https://lohelani.github.io/preliminary-portfolio/
+
+## Screenshots
+![image](https://user-images.githubusercontent.com/70550481/94382875-4f80ab80-010c-11eb-90cc-879974088809.png)
+![image](https://user-images.githubusercontent.com/70550481/94382892-60c9b800-010c-11eb-9f9c-1c0cee0ab872.png)
+![image](https://user-images.githubusercontent.com/70550481/94382900-6a532000-010c-11eb-9557-227502606712.png)
+
---
+++ quiz.js
@@ -0,0 +1,191 @@
+var questions = [
+    {
+        question: "What attribute attaches a script file to an HTML file?",
+        choices: ["src", "href", "br", "type"],
+        answer: "src",
+    },
+    {
+        question:
+            "Values in an array are enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "square brackets",
+    },
+    {
+        question:
+            "What are function parameters listed within?",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses",
+    },
+    {
+        question:
+            "Which of the following would NOT be included in a for loop?",
+        choices: ["i < 5", "i == i", "i ++", "i = 0"],
+        answer: "i == i",
+    },
+    {
+        question:
+            "Which of the following is a boolean data type?",
+        choices: ["YES/NO", "ON/OFF", "TRUE/FALSE", "all of the above"],
+        answer: "all of the above",
+    },
+];
+var startDiv = document.getElementById("start");
+var questionDiv = document.getElementById("question-screen");
+var questionEl = document.querySelector("#question");
+var optionListEl = document.querySelector("#option-list");
+var questionResultEl = document.querySelector("#question-result");
+var timerEl = document.querySelector("#timer");
+var scoreDiv = document.getElementById("scores");
+var finalScore = document.getElementById("final-score");
+var initialsEl = document.getElementById("initials");
+
+var questionIndex = 0;
+var correctCount = 0;
+var time = 25;
+var intervalId;
+//var highscore 
+
+function startQuiz() {
+    startDiv.setAttribute("class", "hide");
+    questionDiv.removeAttribute("class");
+    renderQuestion();
+}
+function endQuiz() {
+    clearInterval(intervalId);
+    scoreDiv.removeAttribute("class");
+    questionDiv.setAttribute("class", "hide");
+
+    // var body = document.body;
+    // body.innerHTML = "Game over: You scored " + correctCount;
+    // body.innerHTML += "<form><label>Initials</label></form>";
+    finalScore.textContent = correctCount;
+    // wait 2 seconds and call showHighScore;
+}
+
+function showHighScore() {
+    var initials = initialsEl.value.trim();
+    var highscores = JSON.parse(localStorage.getItem("highscores")) || [];
+
+    var score = {
+        initials: initials,
+        score: correctCount
+    }
+
+    highscores.push(score);
+    highscores.sort(function (a, b) {
+        return b.score - a.score;
+    });
+    localStorage.setItem("highscores", JSON.stringify(highscores));
+    var score = 0;
+    // var highscore = localStorage.getItem("highscore");
+
+    // if (highscore !== null) {
+    //     if (score > highscore) {
+    //         localStorage.setItem("highscore", score);
+    //     }
+    // }
+    // else {
+    //     localStorage.setItem("highscore", score);
+    //}
+    // display highscores on highscores page
+
+    // document.getElementById("highscoreList").innerHTML =
+    //     "<li>" + highscores.initials.join("</li><li>") + "</li>"
+    // // //("highscores" + "initials")
+    // var scorelist = document.getElementById("highscoreList");
+    // var listItem = scorelist.getElementsByTagName("ul");
+
+    for (var i in highscores) {
+    //     var output = "<div class='meta'>" +
+    //         "<h2>" + highscores[i].initials + "</h2>" 
+    //             "<p>" + highscores[i].score + "</p>" +
+    //             "</div>";
+
+        //var li = highscores[i];
+        var liTag = document.createElement("li");
+        liTag.textContent = highscores.initials + " " + highscores.score;
+        //li.innerHTML = output;
+        console.log(highscores[i]);
+
+        var highscoreList = document.querySelector("highscoreList");
+        highscoreList.innerHTML(liTag);
+    }
+}
+
+
+
+function updateTime() {
+    time--;
+    timerEl.textContent = time;
+    if (time <= 0) {
+        endQuiz();
+    }
+}
+
+
+function renderQuestion() {
+    if (time == 0) {
+        updateTime();
+        return;
+    }
+
+
+    optionListEl.innerHTML = "";
+    questionResultEl.innerHTML = "";
+
+    var choices = questions[questionIndex].choices;
+    var choicesLenth = choices.length;
+
+    intervalId = setInterval(updateTime, 1000);
+    questionEl.textContent = questions[questionIndex].question;
+
+    for (var i = 0; i < choicesLenth; i++) {
+        var questionListItem = document.createElement("li");
+        questionListItem.textContent = choices[i];
+        optionListEl.append(questionListItem);
+    }
+}
+
+function nextQuestion() {
+    questionIndex++;
+    if (questionIndex === questions.length) {
+        endQuiz();
+    } else {
+        renderQuestion();
+    }
+}
+
+function checkAnswer(event) {
+    clearInterval(intervalId);
+    if (event.target.matches("li")) {
+        var answer = event.target.textContent;
+        if (answer === questions[questionIndex].answer) {
+            questionResultEl.textContent = "Correct";
+            correctCount++;
+        } else {
+            questionResultEl.textContent = "Incorrect";
+            time = time - 2;
+            timerEl.textContent = time;
+        }
+    }
+    setTimeout(nextQuestion, 2000);
+
+
+}
+// at end quiz show high scores or go back (restart quiz)
+// renderQuestion();
+var el = document.getElementById("option-list");
+if (el) {
+    el.addEventListener('click', checkAnswer, false);
+}
+var el = document.getElementById("start");
+if (el) {
+    el.addEventListener('click', startQuiz, false);
+}
+var el = document.getElementById("submit");
+if (el) {
+    el.addEventListener('click', showHighScore, false);
+}
+//optionListEl.addEventListener("click", checkAnswer);
+//startDiv.addEventListener("click", startQuiz);
+//document.getElementById("submit").addEventListener("click", showHighScore);
